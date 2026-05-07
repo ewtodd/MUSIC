d5cb45f0e52a1562772cdca3584de0a94527643e
Ethan Todd <30243637+ewtodd@users.noreply.github.com>
2026-05-07 09:39:01 -0500

    rekey

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,9 @@
 let
   ethan-desktop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDlbs+h9OqZMIAC6b3i4tUcXC4PidfBFEQNdwrLS8g9G";
+  ethan-laptop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF5aPPhXY+RssvL9znCFwHjkmUdi4KQkNSnAgd+AQqqx";
   allKeys = [
     ethan-desktop
+    ethan-laptop
   ];
 in
 {
